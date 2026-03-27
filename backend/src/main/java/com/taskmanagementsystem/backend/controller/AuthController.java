f5f69cdb9f5fa31f51c2f0a6d2053bbd0d8b3c1b
Ali Hayek <ahayek50@gmail.com>
2026-03-27 17:10:30 +0200

    Day 2: Implement backend Authentication module with JWT security

---
+++ backend/src/main/java/com/taskmanagementsystem/backend/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.taskmanagementsystem.backend.controller;
+
+import com.taskmanagementsystem.backend.dto.AuthResponse;
+import com.taskmanagementsystem.backend.dto.LoginRequest;
+import com.taskmanagementsystem.backend.dto.RegisterRequest;
+import com.taskmanagementsystem.backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+@RequestMapping("/api/auth")
+
+@CrossOrigin(origins = "*")
+
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(
+            @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
