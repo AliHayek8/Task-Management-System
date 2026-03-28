af328da506460c914ba0c3c9e14d0b791ab385a5
suhailaissa-eng <sosoissa04@gmail.com>
2026-03-28 22:10:04 +0300

    Add pages Routes

--- frontend/taskflow-frontend/src/app/app.routes.ts
+++ frontend/taskflow-frontend/src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { Dashboard } from './features/dashboard/dashboard.component';
+import { ProjectsList } from './features/projects/projects-list/projects-list.component';
+import { Profile } from './features/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
+
   { path: 'auth', component: AuthComponent },
-  { path: 'dashboard', component: Dashboard },
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: 'projects', component: ProjectsList },
+      { path: 'profile', component: Profile },
+    ]
+  },
+
+  { path: '**', redirectTo: 'auth' }
 ];
