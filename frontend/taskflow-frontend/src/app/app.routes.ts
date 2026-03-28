f76ac35b452603a50854f281f875e7bc0f248cc2
suhailaissa-eng <sosoissa04@gmail.com>
2026-03-28 23:16:59 +0300

    Add Projects Paage

--- frontend/taskflow-frontend/src/app/app.routes.ts
+++ frontend/taskflow-frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { Dashboard } from './features/dashboard/dashboard.component';
-import { ProjectsList } from './features/projects/projects-list/projects-list.component';
+import { ProjectsListComponent } from './features/projects/projects-list/projects-list.component';
 import { Profile } from './features/profile/profile.component';
 
 export const routes: Routes = [
@@ -15,7 +15,7 @@ export const routes: Routes = [
     component: MainLayoutComponent,
     children: [
       { path: 'dashboard', component: Dashboard },
-      { path: 'projects', component: ProjectsList },
+      { path: 'projects', component: ProjectsListComponent  },
       { path: 'profile', component: Profile },
     ]
   },
