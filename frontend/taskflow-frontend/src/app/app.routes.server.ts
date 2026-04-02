91a53f4ff6ac1ff973d66246fc081d95a077a62a
Ali Hayek <ahayek50@gmail.com>
2026-04-02 07:04:17 +0300

    fix flash page problem

--- frontend/taskflow-frontend/src/app/app.routes.server.ts
+++ frontend/taskflow-frontend/src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'auth',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'projects',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'projects/:projectId/tasks',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'profile',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
